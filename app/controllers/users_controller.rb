0013bf17207398f2f79197a7f60a6790b7b65913
Jak Major <66277064+jakmajor@users.noreply.github.com>
2021-11-24 10:40:54 -0800

    working on user logins

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+
+    def index 
+        users = User.all 
+        render json: users 
+    end
+
+    def create
+        signup = User.create!(user_params)
+        session[:user_id] = signup.id
+        render json: signup, status: :created
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+    end
+
+    private
+    def user_params
+        params.permit(:name, :username, :image_url, :bio, :password, :password_confirmation)
+    end
+    def authorize
+        return render json: { error: "not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+end
+end
